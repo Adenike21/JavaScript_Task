d053d978f6c745f9a21c1a691fa654e740da07e2
Adenike21 <moriam_t@yahoo.com>
2020-03-24 10:04:48 +0100

    first commit

---
+++ javascript_task1.js
@@ -0,0 +1,11 @@
+//JavaScript first task
+
+let name = "Moriam Adenike Fagbemi";
+var courses = ["HTML", "CSS", "JavaScript"];
+console.log (`My name is: ${name}`);
+console.log (`My Courses are: ${courses}`);
+for (var n = 0; n <= 200; n++){
+    if( n % 2 !== 0){
+        console.log (n);
+    }
+}
